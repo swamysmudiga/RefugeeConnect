884a1c92b0b679381720769d389eef909162370c
swamyms <mudiga.s@northeastern.edu>
2024-04-11 23:28:58 -0400

    Include ContentSection in the MainHomePage file to render ContentSection

---
+++ frontend/refugee-connect/src/components/MainHomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ContentSection from './ContentSection';
+// import NavBar from './NavBar';
+// import SideBar from './SideBar';
+
+const MainPage = () => {
+  return (
+    <div>
+      {/* <NavBar />
+      <SideBar /> */}
+      <ContentSection />
+    </div>
+  );
+};
+
+export default MainPage;
