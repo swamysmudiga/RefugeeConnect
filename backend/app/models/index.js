d776d940b45ab55e498b260187a95032a6a07389
Kishor Satishkumar Kashid <kashid.k@northeastern.edu>
2024-04-13 17:48:09 -0400

    Worked on resources APIs

---
+++ backend/app/models/index.js
@@ -0,0 +1,7 @@
+import login from './login.js'
+import resource from './resource.js'
+
+export default{
+    login,
+    resource
+}
--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,6 +1,8 @@
 import routes from "./LogInRoutes.js";
+import router from './ResourceRoutes.js';
 
 //Initilize the routes
 export const initializeRouter = (app)=>{
     app.use('/refugee',routes);
+    app.use('/resources', router);
 }
