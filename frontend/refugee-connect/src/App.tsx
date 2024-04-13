89bfbc74d97a9b9001ed803caff7edd1243ce0f7
swamyms <mudiga.s@northeastern.edu>
2024-04-13 11:21:22 -0400

    Added Card section in Content Section

--- frontend/refugee-connect/src/App.tsx
+++ frontend/refugee-connect/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import RegistrationForm from './components/RegistrationForm';
-import MainPage from './components/MainHomePage';
+import MainHomePage from './components/MainHomePage';
 
 function App() {
   console.log('App is rendering with RegistrationForm');
   return (
     <div>
       <h1>Welcome to Refugee Connect</h1>
-      <MainPage />
+      <MainHomePage />
 
     </div>
   );
--- frontend/refugee-connect/src/components/ContentSection.tsx
+++ frontend/refugee-connect/src/components/ContentSection.tsx
@@ -4,6 +4,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Facebook, Twitter, LinkedIn } from '@mui/icons-material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
 
 const ContentSection = () => {
   const sliderSettings = {
@@ -15,6 +18,36 @@ const ContentSection = () => {
     autoplay: true,
     autoplaySpeed: 3000
   };
+
+  const countries = [
+    {
+      name: 'Switzerland',
+      continent: 'Europe',
+      image: 'path/to/image1.jpg',
+      description:
+        'Nam exercitationem commodi et ducimus quia in dotore animi sit mollitia amet id quod eligendi. Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium internos.',
+      price: '1,000',
+      going: '25 People Going',
+    },
+    {
+      name: 'Amazon',
+      continent: 'Brazil',
+      image: 'path/to/image2.jpg',
+      description:
+        'Nam exercitationem commodi et ducimus quia in dolore animi sit mollitia amet id quod eligendi. Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium internos.',
+      price: '1,223',
+      going: '830 People Going',
+    },
+    {
+      name: 'Giza',
+      continent: 'Egypt',
+      image: 'path/to/image3.jpg',
+      description:
+        'Nam exercitationem commodi et ducimus quia in dolore animi sit mollitia amet id quod eligendi. Et labore harum non nobis ipsum eum molestias mollitia et corporis praesentium a laudantium internos.',
+      price: '1,200',
+      going: '155 People Going',
+    },
+  ];
 
   return (
     <div>
@@ -32,7 +65,7 @@ const ContentSection = () => {
       <section style={{ backgroundColor: '#808080', padding: '40px 0', marginTop: '40px' }}>
         <Container maxWidth="md">
           <Typography variant="h4" gutterBottom>
-            Another Section
+             Section 1
           </Typography>
           <Slider {...sliderSettings}>
             <div>
@@ -57,35 +90,44 @@ const ContentSection = () => {
           </Slider>
         </Container>
       </section>
-
-      <section style={{ backgroundColor: '#808080', padding: '40px 0', marginTop: '40px' }}>
-        <Container maxWidth="md">
-          <Typography variant="h4" gutterBottom>
-            Another Section
-          </Typography>
-          <Slider {...sliderSettings}>
-            <div>
-              <img src="image1.jpg" alt="Image 1" />
-              <div>
-                <Typography variant="h5">Story 1</Typography>
-                <Typography variant="body1">
-                  Short description of the story 1. <a href="#">Read More</a>
-                </Typography>
-              </div>
-            </div>
-            <div>
-              <img src="image2.jpg" alt="Image 2" />
-              <div>
-                <Typography variant="h5">Story 2</Typography>
-                <Typography variant="body1">
-                  Short description of the story 2. <a href="#">Read More</a>
-                </Typography>
-              </div>
-            </div>
-            {/* Add more slides as needed */}
-          </Slider>
-        </Container>
-      </section>
+      <section className="container mx-auto px-4 py-16" style={{ backgroundColor: '#808080', padding: '40px 0', marginTop: '40px' }}>
+      <div >
+      <Grid container spacing={4}>
+        {countries.map((country) => (
+          <Grid item key={country.name} xs={12} sm={6} md={4}>
+            <Card>
+              <CardActionArea>
+                <img
+                  className="w-full h-40 object-cover"
+                  src={country.image}
+                  alt={country.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {country.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {country.continent}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" mt={2}>
+                    {country.description}
+                  </Typography>
+                  <div className="flex justify-between mt-4">
+                    <Typography variant="body1" component="div">
+                      From ${country.price}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {country.going}
+                    </Typography>
+                  </div>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      </div>
+    </section>
 
       {/* Footer Section */}
       <footer style={{ backgroundColor: '#808080', color: '#fff', padding: '40px 0', marginTop: '40px' }}>
