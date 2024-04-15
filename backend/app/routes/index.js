e958b2a07f36bc1e4cf8dd5fdcb2d2a627571402
swamyms <mudiga.s@northeastern.edu>
2024-04-14 20:54:13 -0400

    Initializing the routing for donation and camp

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -1,6 +1,10 @@
 import routes from "./LogInRoutes.js";
+import donationRoutes from "./donation-routes.js";
+import campRoutes from "./camp-routes.js";
 
 //Initilize the routes
 export const initializeRouter = (app)=>{
     app.use('/refugee',routes);
+    app.use('/donation',donationRoutes);
+    app.use('/camp',campRoutes)
 }
