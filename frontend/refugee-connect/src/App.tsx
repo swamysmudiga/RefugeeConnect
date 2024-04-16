43671c0ee075e04f1469b2d3388be08d7cee10dc
swamyms <mudiga.s@northeastern.edu>
2024-04-16 19:42:14 -0400

    Added  refugeeHomePage, accommodation, food-distribution, medical-clinic path and mapped element component repectively

--- frontend/refugee-connect/src/App.tsx
+++ frontend/refugee-connect/src/App.tsx
@@ -6,8 +6,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Error from './pages/Error';
 import LoginPage from './components/LoginPage';
 import MainHomePage from './components/MainHomePage';
+import RefugeeHomePage from './components/RefugeeHomePage/index';
 import RootLayout from './pages/NavBar';
-
+import Accommodation from './components/RefugeeHomePage/Accomodation';
+import FoodDistributionCenter from './components/RefugeeHomePage/FoodDistributionCenter';
+import MedicalClinic from './components/RefugeeHomePage/MedicialClinic';
 
 const route = createBrowserRouter([
   { 
@@ -16,14 +19,17 @@ const route = createBrowserRouter([
     errorElement:<Error></Error>,
     children:[
       { path:'', element:<MainHomePage/>,},
+      { path:'refugeeHomePage', element:<RefugeeHomePage/>,},
       {  path:'login', element:<LoginPage/> },
       { path:'signup', element:<RegistrationForm/> },
+      { path:'accommodation', element:<Accommodation/> },
+      { path:'food-distribution', element:<FoodDistributionCenter/> },
+      { path:'medical-clinic', element:<MedicalClinic/> }
   
     ]
   },
     
 ]);
-
 
 function App() {
   console.log('App is rendering with RegistrationForm');
