ca1158f0ce9aacea7154830491db46e9e45d969e
swamyms <mudiga.s@northeastern.edu>
2024-04-17 16:49:35 -0400

    Created New Resource in which ViewAllResource and View NearbyCamp section added in refugee home page and added this file in index.tsx to show in RefugeeHomePage

--- frontend/refugee-connect/src/components/RefugeeHomePage/NewResource.tsx
+++ frontend/refugee-connect/src/components/RefugeeHomePage/NewResource.tsx
@@ -2,70 +2,76 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 const ViwAllResourceAndNearbyCamps = () => {
   return (
     <>
       {/* First Container */}
-      <Container maxWidth="xl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 0 }}>
-        <div style={{ width: '40%', padding: '0 2rem' }}>
-          <img
-            className="w-full h-auto rounded-lg object-cover"
-            src="../src/images/Example2.jpeg"
-            alt="Honeymoon destination"
-            width={400}
-          />
-        </div>
-        <div style={{ width: '30%' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
-            <Typography variant="h5" component="div" style={{ textAlign: 'center', marginBottom: '1rem' }}>
-              Resources
-            </Typography>
-            <Typography variant="body1">
-              Our Romantic Tropical Destinations
-            </Typography>
-            <Typography variant="body2" style={{ marginTop: '1rem' }}>
-              Et labore harum non nobis ipsum eum molestias mollitia et
-              corporis praesentium a laudantium internos. Non quis eius quo
-              eligendi corrupti et fugiat nulla qui soluta recusandae in
-              maxime quasi aut ducimus illum aut optio quibusdam!
-            </Typography>
-            <Button variant="contained" style={{ marginTop: '1rem', width: '100%' }}>
-              View All Resources
-            </Button>
-          </div>
-        </div>
+      <Container maxWidth="xl" style={{ padding: '2rem 0' }}>
+        <Grid container spacing={4} alignItems="center">
+          <Grid item xs={12} md={6}>
+            <Box display="flex" justifyContent="center">
+              <img
+                className="w-full h-auto rounded-lg object-cover"
+                src="../src/images/ResourcePhoto.png"
+                alt="resource"
+                style={{ maxWidth: '100%', height: 'auto', width: '400px' }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Typography variant="h4" component="div" style={{ textAlign: 'center', marginBottom: '1rem' }}>
+                Resources
+              </Typography>
+              <Typography variant="body1" style={{ marginBottom: '1rem', marginRight:'4rem' }}>
+                <span>
+                  "View All Resource simplifies access to crucial amenities like
+                </span>
+                <span style={{ fontSize: '1.2rem', fontFamily: 'cursive', color: 'red' }}> accommodation, food distribution centers, and medical clinics.</span>
+                <span> Our user-friendly platform ensures swift access to vital services, facilitating support for those in need."</span>
+              </Typography>
+              <Button variant="contained" style={{ marginTop: '1rem', width: 'auto' }}>
+                View All Resources
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
-      <br />
+
       {/* Second Container with Reversed Content */}
-      <Container maxWidth="xl" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 0 }}>
-        <div style={{ width: '40%' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
-            <Typography variant="h5" component="div" style={{ textAlign: 'center', marginBottom: '1rem' }}>
-              Nearby Camps
-            </Typography>
-            <Typography variant="body1">
-              Our Romantic Tropical Destinations
-            </Typography>
-            <Typography variant="body2" style={{ marginTop: '1rem' }}>
-              Et labore harum non nobis ipsum eum molestias mollitia et
-              corporis praesentium a laudantium internos. Non quis eius quo
-              eligendi corrupti et fugiat nulla qui soluta recusandae in
-              maxime quasi aut ducimus illum aut optio quibusdam!
-            </Typography>
-            <Button variant="contained" style={{ marginTop: '1rem', width: '100%' }}>
-              View Packages
-            </Button>
-          </div>
-        </div>
-        <div style={{ width: '50%', padding: '0 2rem' }}>
-          <img
-            className="w-full h-auto rounded-lg object-cover"
-            src="../src/images/Example2.jpeg"
-            alt="Honeymoon destination"
-            width={400}
-          />
-        </div>
+      <Container maxWidth="xl" style={{ padding: '2rem 0' }}>
+        <Grid container spacing={4} alignItems="center" direction="row-reverse">
+          <Grid item xs={12} md={6}>
+            <Box display="flex" justifyContent="center">
+              <img
+                className="w-full h-auto rounded-lg object-cover"
+                src="../src/images/NearByCamp.png"
+                alt="camp"
+                style={{ maxWidth: '100%', height: 'auto', width: '500px' }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Typography variant="h4" component="div" style={{ textAlign: 'center', marginBottom: '1rem' }}>
+                Nearby Camps
+              </Typography>
+              <Typography variant="body1" style={{ marginBottom: '1rem', marginLeft:'4rem' }}>
+                <span>
+                  Easily find nearby camps through our platform. Click
+                </span>
+                <span style={{ fontSize: '1.2rem', fontFamily: 'cursive', color: 'red' }}> 'View Nearby Camps'</span>
+                <span> for detailed information on locations and amenities.</span>
+              </Typography>
+              <Button variant="contained" style={{ marginTop: '1rem', width: 'auto' }}>
+                View Nearby Camps
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
