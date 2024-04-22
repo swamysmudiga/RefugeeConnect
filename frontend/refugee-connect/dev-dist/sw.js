2fe098cd6de16961bd0fdef33a5deb802c36cf18
kapsep <kapse.p@northeastern.edu>
2024-04-22 09:43:26 -0400

    Worked on View Camp section - changed layout and functionality of text fields

--- frontend/refugee-connect/dev-dist/sw.js
+++ frontend/refugee-connect/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ed931e8e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7s0troe55r8"
+    "revision": "0.f09rasb1icg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
